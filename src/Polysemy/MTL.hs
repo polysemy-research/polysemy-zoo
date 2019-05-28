16409547e64930e55e6d2fa7038bb2e7743793dc
Sandy Maguire <sandy@sandymaguire.me>
2019-05-28 17:13:41 -0400

    try also absorbing reader but no go

--- src/Polysemy/MTL.hs
+++ src/Polysemy/MTL.hs
@@ -4,18 +4,21 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE ScopedTypeVariables   #-}
 {-# LANGUAGE UndecidableInstances  #-}
+{-# OPTIONS_GHC -fplugin=Polysemy.Plugin  #-}
 
 module Polysemy.MTL where
 
 
 import Polysemy
 import Polysemy.State
+import Polysemy.Reader
 import Data.Constraint
 import Data.Constraint.Unsafe
 import Data.Proxy
 import Data.Reflection
 import Data.Semigroup
 import qualified Control.Monad.State.Class as S
+import qualified Control.Monad.Reader.Class as S
 
 newtype Lift1 (p :: (* -> *) -> Constraint) (m :: * -> *) (s) x = Lift1
   { lower1 :: m x
@@ -34,8 +37,34 @@ using d m =
 absorbState :: Member (State s) r => (forall m. S.MonadState s m => m a) -> Sem r a
 absorbState f = using (MonadState get put) f
 
+absorbReader :: Member (Reader i) r => (forall m. S.MonadReader i m => m a) -> Sem r a
+absorbReader f = using (MonadReader ask local) f
+
+carol :: (S.MonadReader Int m, S.MonadState String m) => m Int
+carol = do
+  r <- S.get
+  S.put r
+  S.ask
+
+
+foo
+    :: forall r
+     . ( Member (State String) r
+       , Member (Reader Int) r
+       )
+    => Sem r Int
+foo = absorbState @String @r $ absorbReader @Int @r carol
+
 -- usingT :: forall p f a. ReifiableConstraint p => Def p a -> (p a => f a) -> f a
 -- usingT d m = reify d $ \(_ :: Proxy s) -> m \\ trans (unsafeCoerceConstraint :: (p (Lift p a s) :- p a)) reifiedIns
+
+
+instance ReifiableConstraint1 (S.MonadReader i) where
+  data Def1 (S.MonadReader i) m = MonadReader
+    { ask_ :: m i
+    , local_ :: forall a. (i -> i) -> m a -> m a
+    }
+  reifiedIns1 = Sub Dict
 
 instance ReifiableConstraint1 (S.MonadState s) where
   data Def1 (S.MonadState s) m = MonadState { get_ :: m s, put_ :: s -> m () }
@@ -46,3 +75,10 @@ instance ( Monad m
          ) => S.MonadState s (Lift1 (S.MonadState s) m s') where
   get = Lift1 $ get_ $ reflect $ Proxy @s'
   put s = Lift1 $ put_ (reflect $ Proxy @s') s
+
+instance ( Monad m
+         , Reifies s' (Def1 (S.MonadReader i) m)
+         ) => S.MonadReader i (Lift1 (S.MonadReader i) m s') where
+  ask = Lift1 $ ask_ $ reflect $ Proxy @s'
+  local f m = Lift1 $ local_ (reflect $ Proxy @s') f $ lower1 m
+
