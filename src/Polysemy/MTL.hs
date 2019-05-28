5838e09fdcd6609aa63f416b40df1f105cd70d37
Sandy Maguire <sandy@sandymaguire.me>
2019-05-28 18:21:18 -0400

    It works!

--- src/Polysemy/MTL.hs
+++ src/Polysemy/MTL.hs
@@ -9,16 +9,17 @@
 module Polysemy.MTL where
 
 
-import Polysemy
-import Polysemy.State
-import Polysemy.Reader
-import Data.Constraint
-import Data.Constraint.Unsafe
-import Data.Proxy
-import Data.Reflection
-import Data.Semigroup
-import qualified Control.Monad.State.Class as S
 import qualified Control.Monad.Reader.Class as S
+import qualified Control.Monad.State.Class as S
+import           Data.Constraint
+import           Data.Constraint.Unsafe
+import           Data.Proxy
+import           Data.Reflection
+import           Data.Semigroup
+import           Polysemy
+import           Polysemy.Reader
+import           Polysemy.State
+import Unsafe.Coerce
 
 newtype Lift1 (p :: (* -> *) -> Constraint) (m :: * -> *) (s) x = Lift1
   { lower1 :: m x
@@ -34,30 +35,10 @@ using :: forall p m a. (Monad m, ReifiableConstraint1 p) => Def1 p m -> (p m => 
 using d m =
   reify d $ \(_ :: Proxy s) -> m \\ trans (unsafeCoerceConstraint :: (p (Lift1 p m s) :- p m)) reifiedIns1
 
-absorbState :: Member (State s) r => (forall m. S.MonadState s m => m a) -> Sem r a
-absorbState f = using (MonadState get put) f
 
-absorbReader :: Member (Reader i) r => (forall m. S.MonadReader i m => m a) -> Sem r a
+------------------------------------------------------------------------------
+absorbReader :: Member (Reader i) r => (S.MonadReader i (Sem r) => Sem r a) -> Sem r a
 absorbReader f = using (MonadReader ask local) f
-
-carol :: (S.MonadReader Int m, S.MonadState String m) => m Int
-carol = do
-  r <- S.get
-  S.put r
-  S.ask
-
-
-foo
-    :: forall r
-     . ( Member (State String) r
-       , Member (Reader Int) r
-       )
-    => Sem r Int
-foo = absorbState @String @r $ absorbReader @Int @r carol
-
--- usingT :: forall p f a. ReifiableConstraint p => Def p a -> (p a => f a) -> f a
--- usingT d m = reify d $ \(_ :: Proxy s) -> m \\ trans (unsafeCoerceConstraint :: (p (Lift p a s) :- p a)) reifiedIns
-
 
 instance ReifiableConstraint1 (S.MonadReader i) where
   data Def1 (S.MonadReader i) m = MonadReader
@@ -65,6 +46,17 @@ instance ReifiableConstraint1 (S.MonadReader i) where
     , local_ :: forall a. (i -> i) -> m a -> m a
     }
   reifiedIns1 = Sub Dict
+
+instance ( Monad m
+         , Reifies s' (Def1 (S.MonadReader i) m)
+         ) => S.MonadReader i (Lift1 (S.MonadReader i) m s') where
+  ask = Lift1 $ ask_ $ reflect $ Proxy @s'
+  local f m = Lift1 $ local_ (reflect $ Proxy @s') f $ lower1 m
+
+
+------------------------------------------------------------------------------
+absorbState :: Member (State s) r => (S.MonadState s (Sem r) => Sem r a) -> Sem r a
+absorbState f = using (MonadState get put) f
 
 instance ReifiableConstraint1 (S.MonadState s) where
   data Def1 (S.MonadState s) m = MonadState { get_ :: m s, put_ :: s -> m () }
@@ -76,9 +68,19 @@ instance ( Monad m
   get = Lift1 $ get_ $ reflect $ Proxy @s'
   put s = Lift1 $ put_ (reflect $ Proxy @s') s
 
-instance ( Monad m
-         , Reifies s' (Def1 (S.MonadReader i) m)
-         ) => S.MonadReader i (Lift1 (S.MonadReader i) m s') where
-  ask = Lift1 $ ask_ $ reflect $ Proxy @s'
-  local f m = Lift1 $ local_ (reflect $ Proxy @s') f $ lower1 m
+
+foo :: (S.MonadReader Int m, S.MonadState String m) => m Int
+foo = do
+  r <- S.get
+  S.put r
+  S.ask
+
+
+absorbRS
+    :: forall r
+     . ( Member (State String) r
+       , Member (Reader Int) r
+       )
+    => Sem r Int
+absorbRS = absorbState $ absorbReader foo
 
