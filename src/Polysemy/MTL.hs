abe0f185a78d6ff5a6005b2896715e073f676fe0
adamConnerSax <adam_conner_sax@yahoo.com>
2019-05-30 00:08:35 -0400

    Fixed assorted issues.

--- src/Polysemy/MTL.hs
+++ src/Polysemy/MTL.hs
@@ -1,26 +1,26 @@
+{-# LANGUAGE AllowAmbiguousTypes         #-}
 {-# LANGUAGE ConstraintKinds             #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving  #-}
 {-# LANGUAGE FlexibleInstances           #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving  #-}
 {-# LANGUAGE MultiParamTypeClasses       #-}
 {-# LANGUAGE ScopedTypeVariables         #-}
-{-# LANGUAGE UndecidableInstances        #-}
 {-# LANGUAGE TypeFamilies                #-}
+{-# LANGUAGE UndecidableInstances        #-}
 {-# LANGUAGE UndecidableSuperClasses     #-}
-{-# LANGUAGE AllowAmbiguousTypes         #-}
 {-# OPTIONS_GHC -fplugin=Polysemy.Plugin #-}
 
 module Polysemy.MTL
   (
-  -- * Types
+    -- * Types
     CanonicalEffect
   , ConstrainedAction
   , ReifiableConstraint1
-  , SemDictionary
+  , IsCanonicalEffect
 
-  -- * constraint-polymorphic absorber
+    -- * constraint-polymorphic absorber
   , absorb
   
-  -- * constraint-monomorphic absorbers
+    -- * constraint-monomorphic absorbers
   , absorbReader
   , absorbState
   , absorbWriter
@@ -46,9 +46,11 @@ import           Polysemy.Writer
 import           Polysemy.State
 import           Polysemy.Error
 
-{- | Open type-family mapping a single constraint of the form @(Type -> Type) -> Constraint@, e.g., @MonadState s@,
-to a polysemy effect which can be used to re-interpret that constraint, e.g., 'State s'.
--}
+------------------------------------------------------------------------------
+-- | Open type-family mapping a single constraint of the form
+-- @(Type -> Type) -> Constraint@, e.g., @MonadState s@,
+-- to a polysemy effect which can be used to re-interpret
+-- that constraint, e.g., 'State s'.
 type family CanonicalEffect (p :: (Type -> Type) -> Constraint) :: (Type -> Type) -> Type -> Type
 
 type instance  CanonicalEffect (S.MonadReader env) = Reader env
@@ -57,48 +59,55 @@ type instance  CanonicalEffect (S.MonadState s)    = State s
 type instance  CanonicalEffect (S.MonadError e)    = Error e
 
 
-{- | A newtype wrapper for a monadic action, parameterized by
-a constraint, @p@ on a @(Type -> Type)@ (e.g., a monad); @m@, a specific
-@(Type -> Type)@; and a polysemy effect type-list @r@. With "Data.Reflection"
-we can create instances of @p (ConstrainedAction p m r)@ using functions from
-@Sem r@.
--}
-newtype ConstrainedAction (p :: (Type -> Type) -> Constraint) (m :: Type -> Type) (s :: Type) (x :: Type)
+-- | A newtype wrapper for a monadic action, parameterized by
+-- a constraint, @p@ on a @(Type -> Type)@ (e.g., a monad); @m@, a specific
+-- @(Type -> Type)@; and a polysemy effect type-list @r@. With "Data.Reflection"
+-- we can create instances of @p (ConstrainedAction p m r)@ using functions from
+-- @Sem r@.
+newtype ConstrainedAction (p :: (Type -> Type) -> Constraint)
+                          (m :: Type -> Type)
+                          (s :: Type)
+                          (x :: Type)
   = ConstrainedAction
     { action :: m x
     } deriving (Functor, Applicative, Monad)
 
-{- | For a constraint to be "absorbable" by @Sem r@, there needs to be an instance of this class,
-containing the dictionary signatures as a record of functions and the
-reflected entailment of @p (ConstrainedAction p m r)@ from the reified dictionary.
--}
+-- | For a constraint to be "absorbable" by @Sem r@,
+-- there needs to be an instance of this class,
+-- containing the dictionary signatures as a record of functions and the
+-- reflected entailment of @p (ConstrainedAction p m r)@ from the reified dictionary.
 class ReifiableConstraint1 p where
   data Dict (p :: (Type -> Type) -> Constraint) (m :: Type -> Type)
   reifiedInstance :: Monad m => R.Reifies s (Dict p m) :- p (ConstrainedAction p m s)
 
-{- | This class contains an instance of the dictionary for some set of effects
-parameterized by a polysemt effect list @r@.
-Typically, you would write this instance for any @r@
-satisfying the constraint that the "canonical" effect is a member.  But you
-could also use it to discharge constraints which require multiple polysemy effects.
--}
-class ReifiableConstraint1 p => SemDictionary p r where
-  semDictionary :: Dict p (Sem r)
+-- | This class contains an instance of the dictionary for some set of effects
+-- parameterized by a polysemt effect list @r@.
+-- Typically, you would write this instance for any @r@
+-- satisfying the constraint that the "canonical" effect is a member.  But you
+-- could also use it to discharge constraints which require multiple polysemy effects.
+
+class ReifiableConstraint1 p => IsCanonicalEffect p r where
+  canonicalDictionary :: Dict p (Sem r)
 
 -- | Given a reifiable constraint, and a dictionary to use, discharge the constraint.
-using :: forall p m a. (Monad m, ReifiableConstraint1 p) => Dict p m -> (p m => m a) -> m a
+using :: forall p m a. (Monad m, ReifiableConstraint1 p)
+  => Dict p m -> (p m => m a) -> m a
 using d m =
-  R.reify d $ \(_ :: Proxy s) -> m \\ C.trans (C.unsafeCoerceConstraint :: (p (ConstrainedAction p m s) :- p m)) reifiedInstance
+  R.reify d $ \(_ :: Proxy s) -> m \\ C.trans
+  (C.unsafeCoerceConstraint :: ((p (ConstrainedAction p m s) :- p m))) reifiedInstance
+{-# INLINEABLE using #-}
 
-{- | Given a "canonical" dictionary for @p@ using the polysemy effects in @r@,
-discharge the constraint @p@.
--}
-absorb :: forall p r a. SemDictionary p r => (p (Sem r) => Sem r a) -> Sem r a
-absorb = (using @p) semDictionary
+-- | Given a "canonical" dictionary for @p@ using the polysemy effects in @r@,
+-- discharge the constraint @p@.
+absorb :: forall p r a. IsCanonicalEffect p r => (p (Sem r) => Sem r a) -> Sem r a
+absorb = using @p canonicalDictionary
+{-# INLINEABLE absorb #-}
 
 ------------------------------------------------------------------------------
-absorbReader :: forall i r a. Member (Reader i) r => (S.MonadReader i (Sem r) => Sem r a) -> Sem r a
-absorbReader f = absorb @(S.MonadReader i) f --using (MonadReader ask local) f
+absorbReader :: Member (Reader i) r
+  => (S.MonadReader i (Sem r) => Sem r a) -> Sem r a
+absorbReader = absorb @(S.MonadReader _)
+{-# INLINEABLE absorbReader #-}
 
 instance ReifiableConstraint1 (S.MonadReader i) where
   data Dict (S.MonadReader i) m = MonadReader
@@ -111,14 +120,18 @@ instance ( Monad m
          , R.Reifies s' (Dict (S.MonadReader i) m)
          ) => S.MonadReader i (ConstrainedAction (S.MonadReader i) m s') where
   ask = ConstrainedAction $ ask_ $ R.reflect $ Proxy @s'
+  {-# INLINEABLE ask #-}
   local f m = ConstrainedAction $ local_ (R.reflect $ Proxy @s') f $ action m
-
-instance Member (Reader i) r => SemDictionary (S.MonadReader i) r where
-  semDictionary = MonadReader ask local
+  {-# INLINEABLE local #-}
   
+instance Member (Reader i) r => IsCanonicalEffect (S.MonadReader i) r where
+  canonicalDictionary = MonadReader ask local
+  {-# INLINEABLE canonicalDictionary #-}
 ------------------------------------------------------------------------------
-absorbState :: forall s r a. Member (State s) r => (S.MonadState s (Sem r) => Sem r a) -> Sem r a
-absorbState f = absorb @(S.MonadState s) f -- using (MonadState get put) f
+absorbState :: Member (State s) r
+  => (S.MonadState s (Sem r) => Sem r a) -> Sem r a
+absorbState = absorb @(S.MonadState _) -- using (MonadState get put) f
+{-# INLINEABLE absorbState #-}
 
 instance ReifiableConstraint1 (S.MonadState s) where
   data Dict (S.MonadState s) m = MonadState
@@ -131,15 +144,19 @@ instance ( Monad m
          , R.Reifies s' (Dict (S.MonadState s) m)
          ) => S.MonadState s (ConstrainedAction (S.MonadState s) m s') where
   get = ConstrainedAction $ get_ $ R.reflect $ Proxy @s'
+  {-# INLINEABLE get #-}  
   put s = ConstrainedAction $ put_ (R.reflect $ Proxy @s') s
+  {-# INLINEABLE put #-}
 
-
-instance Member (State s) r => SemDictionary (S.MonadState s) r where
-  semDictionary = MonadState get put
-
+instance Member (State s) r => IsCanonicalEffect (S.MonadState s) r where
+  canonicalDictionary = MonadState get put
+  {-# INLINEABLE canonicalDictionary #-}
+  
 --------------------------------------------------------------------------------
-absorbWriter :: forall w r a. (Monoid w, Member (Writer w) r) => (S.MonadWriter w (Sem r) => Sem r a) -> Sem r a
-absorbWriter = absorb @(S.MonadWriter w)
+absorbWriter :: (Monoid w, Member (Writer w) r)
+  => (S.MonadWriter w (Sem r) => Sem r a) -> Sem r a
+absorbWriter = absorb @(S.MonadWriter _)
+{-# INLINEABLE absorbWriter #-}
 
 instance Monoid w => ReifiableConstraint1 (S.MonadWriter w) where
   data Dict (S.MonadWriter w) m = MonadWriter
@@ -154,23 +171,29 @@ instance ( Monad m
          , R.Reifies s' (Dict (S.MonadWriter w) m)
          ) => S.MonadWriter w (ConstrainedAction (S.MonadWriter w) m s') where
   tell w = ConstrainedAction $ tell_ (R.reflect $ Proxy @s') w
+  {-# INLINEABLE tell #-}  
   listen x = ConstrainedAction $ listen_ (R.reflect $ Proxy @s') (action x)
+  {-# INLINEABLE listen #-}  
   pass x = ConstrainedAction $ pass_ (R.reflect $ Proxy @s') (action x)
+  {-# INLINEABLE pass #-}  
 
 {- This one requires a little work since the polysemy writer is a bit different from the
 mtl-standard one
 -} 
-instance (Monoid w, Member (Writer w) r) => SemDictionary (S.MonadWriter w) r where
-  semDictionary = MonadWriter tell semListen semPass where
+instance (Monoid w, Member (Writer w) r) => IsCanonicalEffect (S.MonadWriter w) r where
+  canonicalDictionary = MonadWriter tell semListen semPass where
     semListen = fmap (\(x,y) -> (y,x)) . listen
     semPass :: Member (Writer w) r => Sem r (a, w -> w) -> Sem r a 
     semPass x = do
-      (a, f) <- x
-      censor f (return a)
-
+      (w, (a, f)) <- listen x
+      censor f (tell w >> pure a)
+  {-# INLINEABLE canonicalDictionary #-}
+  
 --------------------------------------------------------------------------------
-absorbError :: forall e r a. Member (Error e) r => (S.MonadError e (Sem r) => Sem r a) -> Sem r a
+absorbError :: forall e r a. Member (Error e) r
+  => (S.MonadError e (Sem r) => Sem r a) -> Sem r a
 absorbError = absorb @(S.MonadError e)
+{-# INLINEABLE absorbError #-}
 
 instance ReifiableConstraint1 (S.MonadError e) where
   data Dict (S.MonadError e) m = MonadError
@@ -183,7 +206,10 @@ instance ( Monad m
          , R.Reifies s' (Dict (S.MonadError e) m)
          ) => S.MonadError e (ConstrainedAction (S.MonadError e) m s') where
   throwError e = ConstrainedAction $ throwError_ (R.reflect $ Proxy @s') e
+  {-# INLINEABLE throwError #-}
   catchError x f = ConstrainedAction $ catchError_ (R.reflect $ Proxy @s') (action x) (action . f)
-
-instance Member (Error e) r => SemDictionary (S.MonadError e) r where
-  semDictionary = MonadError throw catch 
+  {-# INLINEABLE catchError #-}
+  
+instance Member (Error e) r => IsCanonicalEffect (S.MonadError e) r where
+  canonicalDictionary = MonadError throw catch 
+  {-# INLINEABLE canonicalDictionary #-}
--- test/MTLSpec.hs
+++ test/MTLSpec.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE OverloadedStrings   #-}
-{-# LANGUAGE TypeApplications    #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications    #-}
 module MTLSpec where
 
 import           Polysemy
@@ -12,10 +12,8 @@ import           Polysemy.MTL
 
 import qualified Data.Text                     as T
 import           Test.Hspec
+import           Control.Monad                 as M
 
-import           Control.Monad                  ( replicateM_
-                                                , when
-                                                )
 import qualified Control.Monad.Reader.Class    as S
 import qualified Control.Monad.Writer.Class    as S
 import qualified Control.Monad.State.Class     as S
@@ -31,7 +29,7 @@ getEnvLength :: S.MonadReader T.Text m => m Int
 getEnvLength = S.ask >>= return . T.length
 
 replicateTell :: S.MonadWriter [Int] m => Int -> Int -> m ()
-replicateTell n m = replicateM_ n $ S.tell [m]
+replicateTell n m = M.replicateM_ n $ S.tell [m]
 
 retrieveAndUpdateN :: S.MonadState Int m => Int -> m Int
 retrieveAndUpdateN n = do
@@ -42,7 +40,7 @@ retrieveAndUpdateN n = do
 -- this one is exceptionally boring
 throwOnZero :: S.MonadError T.Text m => Int -> m Int
 throwOnZero n = do
-  when (n == 0) $ S.throwError "Zero!"
+  M.when (n == 0) $ S.throwError "Zero!"
   return n
 
 someOfAll
@@ -52,92 +50,103 @@ someOfAll = do
   n <- S.get
   S.tell [n]
   S.ask
+------------------------------------------------------------------------------
 
 spec :: Spec
-spec = do
-  describe "MTL" $ do
-
-    it
-        "should return 10, the sum of lengths of the strings provided to run (\"Text1\") and then to local (\"Text2\")"
-      $ do
-          flip shouldBe 10 $ run . runReader "Text1" $ do
-            a <- absorbReader getEnvLength
-            b <- local (const "Text2") $ absorbReader getEnvLength
-            return (a + b)
-
-    it
-        "should return the sum, after censoring, of all things told.  In this case, 16, the sum of \"init [1,5,5,5,5]\""
-      $ do
-          flip shouldBe 16 $ sum . fst . run . runWriter $ do
-            tell [1]
-            absorbWriter $ replicateTell 2 5
-            censor init $ absorbWriter $ replicateTell 2 5
-
-    it "Should return 0 (since 10 - (20 `div` 2) = 0)"
-      $ flip shouldBe 0
-      $ fst
-      . run
-      . runState 0
-      $ do
-          put 20
-          n <- absorbState $ retrieveAndUpdateN 10
-          modify (\m -> m - (n `div` 2))
-          return ()
-
-    it "should return (Left \"Zero!\")."
-      $ flip shouldBe (Left "Zero!")
-      $ run
-      . runError
-      $ absorbError
-      $ throwOnZero 0
-
-    let runRWS
-          :: T.Text
-          -> Int
-          -> Sem '[Reader T.Text, State Int, Writer [Int]] a
-          -> ([Int], (Int, a))
-        runRWS env0 s0 = run . runWriter . runState s0 . runReader env0
-
-    it "All of them, singly"
-      $ flip shouldBe ([20, 20], (10, 6))
-      $ runRWS "RunAll" 0
-      $ do
-          put 20
-          n <- absorbState $ retrieveAndUpdateN 10
-          absorbWriter $ replicateTell 2 n
+spec = describe "MTL" $ do
+  it
+      (  "should absorb reader twice, thus returning 9, "
+      ++ "the sum of lengths of the strings provided to run (\"Text\")"
+      ++ " and then to local (\"Text2\")"
+      )
+    $ do
+        flip shouldBe 9 . run . runReader "Text" $ do
           a <- absorbReader getEnvLength
-          return a
+          b <- local (const "Text2") $ absorbReader getEnvLength
+          return (a + b)
 
-    let
-      absorbRWS
-        :: forall env w s r a
-         . (Monoid w, Members '[Reader env, Writer w, State s] r)
-        => (  ( S.MonadReader env (Sem r)
-             , S.MonadWriter w (Sem r)
-             , S.MonadState s (Sem r)
-             )
-           => Sem r a
+  it
+      (  "should return the sum, after censoring, of all things told."
+      ++ " In this case, 16, the sum of \"init [1,5,5,5,5]\""
+      )
+    $ do
+        flip shouldBe 16 . sum . fst . run . runWriter $ do
+          tell [1]
+          absorbWriter $ replicateTell 2 5
+          censor init $ absorbWriter $ replicateTell 2 5
+
+  it "same as above but with absorbWriter on the outside of the do block" $ do
+    flip shouldBe 16 . sum . fst . run . runWriter $ absorbWriter $ do
+      S.tell [1]
+      replicateTell 2 5
+      S.pass $ do
+        x <- replicateTell 2 5
+        return (x, init)
+
+  it "Should return 0 (since 10 - (20 `div` 2) = 0)" $ do
+    flip shouldBe 0 . fst . run . runState 0 $ do
+      put 20
+      n <- absorbState $ retrieveAndUpdateN 10
+      modify (\m -> m - (n `div` 2))
+      return ()
+
+  it "should return (Left \"Zero!\")." $ do
+    flip shouldBe (Left "Zero!") . run . runError $ absorbError $ throwOnZero 0
+
+  let runRWS
+        :: T.Text
+        -> Int
+        -> Sem '[Reader T.Text, State Int, Writer [Int]] a
+        -> ([Int], (Int, a))
+      runRWS env0 s0 = run . runWriter . runState s0 . runReader env0
+
+  it "All of them, singly" $ do
+    flip shouldBe ([20, 20], (10, 6)) . runRWS "RunAll" 0 $ do
+      put 20
+      n <- absorbState $ retrieveAndUpdateN 10
+      absorbWriter $ replicateTell 2 n
+      a <- absorbReader getEnvLength
+      return a
+
+  let
+    absorbRWS
+      :: (Monoid w, Members '[Reader env, Writer w, State s] r)
+      => (  ( S.MonadReader env (Sem r)
+           , S.MonadWriter w (Sem r)
+           , S.MonadState s (Sem r)
            )
-        -> Sem r a
-      absorbRWS x =
-        absorb @(S.MonadReader env)
-          $ absorb @(S.MonadWriter w)
-          $ absorb @(S.MonadState s) x
+         => Sem r a
+         )
+      -> Sem r a
+    absorbRWS x = absorbReader $ absorbWriter $ absorbState x
 
-    it "All of them, one absorber"
-      $ flip shouldBe ([20, 20], (10, 6))
-      $ runRWS "RunAll" 0
-      $ do
-          put 20
-          n <- absorbRWS $ retrieveAndUpdateN 10
-          absorbRWS $ replicateTell 2 n
-          a <- absorbRWS getEnvLength
-          return a
+  it "All of them, one absorber" $ do
+    flip shouldBe ([20, 20], (10, 6)) . runRWS "RunAll" 0 $ do
+      put 20
+      n <- absorbRWS $ retrieveAndUpdateN 10
+      absorbRWS $ replicateTell 2 n
+      a <- absorbRWS getEnvLength
+      return a
 
-    it "absorb a stack"
-      $ flip shouldBe ([10, 20], (20, "RunAll"))
-      $ runRWS "RunAll" 0
+  it "All of them, one absorber, absorbRWS outside do block." $ do
+    flip shouldBe ([20, 20], (10, 6)) . runRWS "RunAll" 0 $ absorbRWS $ do
+      S.put 20
+      n <- retrieveAndUpdateN 10
+      replicateTell 2 n
+      a <- getEnvLength
+      return a
+
+  it "absorb a stack" $ do
+    flip shouldBe ([10, 20], (20, "RunAll")) . runRWS "RunAll" 0 $ do
+      put 20
+      tell [10]
+      absorbRWS someOfAll
+
+  it "absorb a stack, absorb outside do." $ do
+    flip shouldBe ([10, 20], (20, "RunAll"))
+      . runRWS "RunAll" 0
+      $ absorbRWS
       $ do
-          put 20
-          tell [10]
-          absorbRWS someOfAll
+          S.put 20
+          S.tell [10]
+          someOfAll
