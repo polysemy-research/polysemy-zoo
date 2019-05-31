fd96d9d4050b87184fdac2b138c9f5a57eaf3b6d
adamConnerSax <adam_conner_sax@yahoo.com>
2019-05-30 21:01:48 -0400

    Added an absorber test and a runRandomIO test. Sort of.

--- test/RandomFuSpec.hs
+++ test/RandomFuSpec.hs
@@ -11,14 +11,15 @@ import           Control.Monad                 as M
 import           Control.Monad.IO.Class         ( liftIO )
 
 import qualified Data.Random                   as R
---import qualified Data.Random.Source            as R
---import qualified Data.Random.Internal.Source   as R
 import qualified Data.Random.Source.PureMT     as R
---import qualified System.Random                 as SR
 
 getRandomInts :: Member RandomFu r => Int -> Sem r [Int]
 getRandomInts nDraws =
   sampleRVar $ M.replicateM nDraws (R.uniform 0 (100 :: Int))
+
+getRandomIntsMR :: R.MonadRandom m => Int -> m [Int]
+getRandomIntsMR nDraws =
+  R.sample $ M.replicateM nDraws (R.uniform 0 (100 :: Int))
 
 ------------------------------------------------------------------------------
 
@@ -28,4 +29,18 @@ spec = describe "RandomFu" $ do
       "Should produce [3, 78, 53, 41, 56], 5 psuedo-random Ints seeded from the same seed on each test."
     $   (runM . runRandomIOPureMT (R.pureMT 1) $ getRandomInts 5)
     >>= (`shouldBe` [3, 78, 53, 41, 56])
+
+  it
+      "Should produce [3, 78, 53, 41, 56], 5 psuedo-random Ints seeded from the same seed on each test. Absorbing MonadRandom."
+    $   ( runM
+        . runRandomIOPureMT (R.pureMT 1)
+        $ absorbMonadRandom
+        $ getRandomIntsMR 5
+        )
+    >>= (`shouldBe` [3, 78, 53, 41, 56])
+
+
+  it "Should produce 5 psuedo-random Ints."
+    $   (fmap length . runM . runRandomIO $ getRandomInts 5)
+    >>= (`shouldBe` 5)
 
