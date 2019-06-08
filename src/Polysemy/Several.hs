9951206fd92b18852e5ca99f35b2f3191f155dde
Jackson Garner <jkrmnj465@gmail.com>
2019-06-08 17:16:24 -0600

    Add interpreters for Reader and State based on HList (#18)
    * Add interpreters for Reader and State based on HList
    
    Normally, you would need to compose tons of runReader and runState
    functions for each type in your effect. runReaders and runStates
    are new functions which allow you to pass in an Hlist
    and automatically interpret all of the values inside. For reader
    or state heavy effects, this should cut down on boilerplate.
    
    The Readers and States types are designed to decrease
    boilerplate in type signatures in similar circumstances.
    
    * Add runSeveral as an abstraction over run*s
    
    The run*s functions are now implemented as partial application on
    runSeveral. RunConstInputs was also added.
    
    * Export TypeMap and TypeConcat to go with runSeveral
    
    Users may want to create their own specializations for runSeveral.
    If they do, they may want to give their specializations type
    signatures. Doing that requires having those type families so this
    adds them to the export list.
    
    * Remove specialized functions
    
    * Change name to Several

---
+++ src/Polysemy/Several.hs
@@ -0,0 +1,73 @@
+{-# LANGUAGE BlockArguments  #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+module Polysemy.Several
+        ( -- * Data
+          HList(..)
+        , TypeMap
+        , TypeConcat
+        , runSeveral
+        )
+where
+
+import Polysemy
+import Data.Kind
+
+------------------------------------------------------------------------------
+-- | A list capable of storing values of different types. Like the Sem type,
+-- it uses a type level list to keep track of what's stored inside. Creating an
+-- HList looks like:
+--
+-- > 1 ::: "test" ::: True ::: HNil
+infixr 5 :::
+data HList a where
+    HNil  :: HList '[]
+    (:::) :: a -> HList (b :: [Type]) -> HList (a ': b)
+
+type Eff = (Type -> Type) -> Type -> Type
+------------------------------------------------------------------------------
+-- | A map function over type level lists. For example, the following two
+-- lines are equivalent:
+--
+-- > TypeMap Reader [Int, String, False]
+-- > [Reader Int, Reader String, Reader Bool]
+type family TypeMap (f :: a -> b) (xs :: [a]) where
+    TypeMap _ '[]       = '[]
+    TypeMap f (x ': xs) = f x ': TypeMap f xs
+
+------------------------------------------------------------------------------
+-- | Like ++ but at the type level. The following two lines are equivalent:
+--
+-- > TypeConcat [Int, String] [Bool]
+-- > [Int, String, Bool]
+type family TypeConcat (a :: [t]) (b :: [t]) where
+    TypeConcat '[] b = b
+    TypeConcat (a ': as) b = a ': TypeConcat as b
+
+------------------------------------------------------------------------------
+-- | A helper function for building new runners which accept HLists intsead of
+-- individual elements. If you would normally write
+--
+-- > f 5 . f "Text" . f True
+--
+-- then this function can turn that into
+--
+-- > runSeveral f (True ::: "Text" ::: 5 ::: HNil)
+--
+-- For example, a runReaders function could be implemented as:
+--
+-- > runReaders :: HList t -> Sem (TypeConcat (TypeMap Reader t) r) a -> Sem r a
+-- > runReaders = runSeveral runReader
+--
+-- Likewise, runStates could be the following if you didn't want the returned
+-- state:
+--
+-- > runStates :: HList t -> Sem (TypeConcat (TypeMap State t) r) a -> Sem r a
+-- > runStates = runSeveral (fmap (fmap snd) . runState)
+runSeveral
+    :: (forall r k x. k -> Sem (e k ': r) x -> Sem r x)
+    -> HList t
+    -> Sem (TypeConcat (TypeMap e t) r) a
+    -> Sem r a
+runSeveral f (a ::: as) = runSeveral f as . f a
+runSeveral _ HNil       = id
---
+++ test/SeveralSpec.hs
@@ -0,0 +1,78 @@
+module SeveralSpec where
+
+import           Polysemy
+import           Polysemy.State
+import           Polysemy.Reader
+import           Polysemy.Input
+import           Polysemy.Several
+import           Test.Hspec
+
+
+readerProgram :: ( Member (Reader String) r
+                 , Member (Reader Int) r
+                 , Member (Reader Bool) r
+                 , Members [Reader String, Reader Int, Reader Bool] r
+                     ~ Members (TypeMap Reader [String, Int, Bool]) r
+                 ) => Sem r (Int, String, Bool)
+readerProgram = do
+  a <- ask @Int
+  b <- ask @String
+  c <- ask @Bool
+  pure $ (a, b, c)
+
+stateProgram :: ( Member (State String) r
+                , Member (State Int) r
+                , Member (State Bool) r
+                , Members [State String, State Int, State Bool] r
+                    ~ Members (TypeMap State [String, Int, Bool]) r
+                ) => Sem r (Int, String, Bool)
+stateProgram = do
+  put @Int 5
+  put "Changed"
+  a <- get @Int
+  b <- get @String
+  c <- get @Bool
+  pure $ (a, b, c)
+
+inputProgram :: ( Member (Input String) r
+                , Member (Input Int) r
+                , Member (Input Bool) r
+                , Members [Input String, Input Int, Input Bool] r
+                    ~ Members (TypeMap Input [String, Int, Bool]) r
+                ) => Sem r (Int, String, Bool)
+inputProgram = do
+  a <- input @Int
+  b <- input @String
+  c <- input @Bool
+  pure $ (a, b, c)
+
+
+runReaders = runSeveral runReader
+
+runStates = runSeveral (fmap (fmap snd) . runState)
+
+runConstInputs = runSeveral runConstInput
+
+spec = do
+  describe "runReaders" $ do
+    let original = runReader 5 . runReader "test" . runReader True $ readerProgram
+        new = runReaders (True ::: "test" ::: 5 ::: HNil) readerProgram
+
+    it "should be equivalent to composed runReader" $ do
+      run original `shouldBe` run new
+
+  describe "runStates" $ do
+    let getResult = fmap $ snd . snd . snd
+        original = getResult $ runState 5 . runState "test" . runState True $ stateProgram
+        new = runStates (True ::: "test" ::: 5 ::: HNil) stateProgram
+
+    it "should be equivalent to composed runState" $ do
+      run original `shouldBe` run new
+
+  describe "runConstInput" $ do
+    let original = runConstInput 5 . runConstInput "test"
+                                   . runConstInput True $ inputProgram
+        new = runConstInputs (True ::: "test" ::: 5 ::: HNil) inputProgram
+
+    it "should be equivalent to composed runConstInput" $ do
+      run original `shouldBe` run new
