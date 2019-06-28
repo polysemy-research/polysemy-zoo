2987e2f9394e7ad509b7a86a4f37745fdafad1da
Sandy Maguire <sandy@sandymaguire.me>
2019-06-28 16:41:32 -0400

    Floodgate effect (#29)

---
+++ test/FloodgateSpec.hs
@@ -0,0 +1,31 @@
+module FloodgateSpec where
+
+import Test.Hspec
+import Polysemy
+import Polysemy.Floodgate
+import Polysemy.Trace
+
+spec :: Spec
+spec = describe "Floodgate" $ do
+  it "should delay held traces until release" $ do
+    let (ts, n) = run . runTraceAsList . runFloodgate $ do
+          hold $ trace "first1"
+          hold $ trace "first2"
+          trace "not held"
+          hold $ trace "second"
+          trace "not held again"
+          hold $ trace "third"
+          release
+          trace "finished"
+          pure $ id @Int 17
+
+    n `shouldBe` 17
+    ts `shouldBe`
+      [ "not held"
+      , "not held again"
+      , "first1"
+      , "first2"
+      , "second"
+      , "third"
+      , "finished"
+      ]
