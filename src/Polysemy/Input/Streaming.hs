abc9c60bb01f78bc495280b8c018d07e8a8d7c31
Sandy Maguire <sandy@sandymaguire.me>
2019-11-12 14:12:53 +0700

    also add yieldRace

--- src/Polysemy/Input/Streaming.hs
+++ src/Polysemy/Input/Streaming.hs
@@ -1,7 +1,9 @@
 module Polysemy.Input.Streaming where
 
+import qualified Control.Concurrent.Async as A
 import           Data.Functor.Of
 import           Polysemy
+import           Polysemy.Final
 import           Polysemy.Input
 import           Polysemy.State
 import qualified Streaming as S
@@ -41,6 +43,30 @@ runInputViaInfiniteStream stream
   )
 
 
+yieldRace
+    :: Members
+        '[ Final IO
+         , Input i1
+         , Input i2
+         ] r
+    => S.Stream (S.Of (Either i1 i2)) (Sem r) ()
+yieldRace = do
+  z <- S.lift $ withStrategicToFinal $ do
+         input1 <- runS input
+         input2 <- runS input
+         pure $ fmap sequenceEither $ A.race input1 input2
+  S.yield z
+
+
+sequenceEither :: Functor f => Either (f a) (f b) -> f (Either a b)
+sequenceEither (Left fa) = Left <$> fa
+sequenceEither (Right fb) = Right <$> fb
+
+
 yieldInput :: Member (Input i) r => S.Stream (Of i) (Sem r) ()
 yieldInput = S.lift input >>= S.yield
+
+
+exhaust :: Member (Input i) r => S.Stream (Of i) (Sem r) a
+exhaust = S.repeatM input
 
