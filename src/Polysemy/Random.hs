331d99038f39a73c4632d42103d97f3d08fb0e2d
Mako Bates <33739499+ShapeOfMatter@users.noreply.github.com>
2023-07-10 20:32:28 -0400

    add helper functions for Random

--- src/Polysemy/Random.hs
+++ src/Polysemy/Random.hs
@@ -11,8 +11,18 @@ module Polysemy.Random
     -- * Interpretations
   , runRandom
   , runRandomIO
+
+    -- * Helpers
+  , distributed
+  , oneOf
+  , sample
+  , sampleR
+  , weighted
   ) where
 
+import           Data.List (genericReplicate)
+import           Data.List.NonEmpty as NonEmpty ((!!), NonEmpty((:|)))
+import           Numeric.Natural (Natural)
 import           Polysemy
 import           Polysemy.State
 import qualified System.Random as R
@@ -20,7 +30,9 @@ import qualified System.Random as R
 ------------------------------------------------------------------------------
 -- | An effect capable of providing 'R.Random' values.
 data Random m a where
+  -- | Yield a value, randomly sampled from the uniform distribution over all values of the given type.
   Random :: R.Uniform x => Random m x
+  -- | Yield a value, randomly sampled from the uniform distribution over the given inclusive range.
   RandomR :: R.UniformRange x => (x, x) -> Random m x
 
 makeSem ''Random
@@ -54,4 +66,75 @@ runRandomIO m = do
   q <- embed @IO R.newStdGen
   snd <$> runRandom q m
 {-# INLINE runRandomIO #-}
+
+
+------------------------------------------------------------------------------
+-- | Pick (uniformly) randomly from a (finite) non-empty list.
+oneOf :: forall a r.
+         (Member Random r) =>
+         NonEmpty a -> Sem r a
+oneOf xs = do i <- randomR (0, length xs - 1)
+              return $ xs NonEmpty.!! i
+
+------------------------------------------------------------------------------
+-- | Pick randomly from a finite non-empty list, using weight annotations.
+-- Behavior is undefined if all weights are zero.
+weighted :: forall a r.
+            (Member Random r) =>
+            NonEmpty (Natural, a) -> Sem r a
+weighted xs = consume xs <$> randomR (0, (sum $ fst <$> xs) - 1)
+{-weighted ((w0, x0) :| xs) = do
+  let weightedXs = scanl1 (\(acc, _) (w, x) -> (t + w, x)) xs
+      total = case weightedXs of
+        _ : _ -> w0 + (fst $ last weightedXs)
+        []    -> w0
+  i <- randomR (0, total)
+  let result = dropWhile (\(w, _) -> w <= i) $ weightedXs
+  return case result of
+    (_, x) : _ -> x
+    _ -> x0-}
+
+------------------------------------------------------------------------------
+-- | Pick randomly from a non-empty possibly-infinite list, using normalized weight annotations.
+-- The requirement that all weights be 0-1 (inclusive) and that they sum to 1 is not checked!
+distributed :: forall a w r.
+               (Num w
+               ,Ord w
+               ,R.UniformRange w
+               ,Member Random r) =>
+               NonEmpty (w, a) -> Sem r a
+distributed xs = consume xs <$> randomR (0, 1)
+
+
+consume :: (Num w, Ord w) => NonEmpty (w, a) -> w -> a
+consume ((_, x) :| []) _ = x
+consume ((weight, x) :| (x' : xs)) threshold | threshold < weight = x
+                                             | otherwise          = consume (x' :| xs) (threshold - weight)
+
+  {-let weightedXs = scanl1 (\(acc, _) (w, x) -> (t + w, x)) xs
+      total = case weightedXs of
+        _ : _ -> w0 + (fst $ last weightedXs)
+        []    -> w0
+  let result = dropWhile (\(w, _) -> w <= i) $ weightedXs
+  return case result of
+    (_, x) : _ -> x
+    _ -> x0-}
+
+------------------------------------------------------------------------------
+-- | Generate n random values.
+sample :: forall a i r.
+          (Integral i,
+           R.Uniform a,
+           Member Random r) =>
+          i -> Sem r [a]
+sample n = sequence . genericReplicate n $ random
+
+------------------------------------------------------------------------------
+-- | Generate n random values in a range.
+sampleR :: forall a i r.
+           (Integral i,
+            R.UniformRange a,
+            Member Random r) =>
+           i -> (a, a) -> Sem r [a]
+sampleR n r = sequence . genericReplicate n $ randomR r
 
--- test/RandomSpec.hs
+++ test/RandomSpec.hs
@@ -1,5 +1,7 @@
 module RandomSpec where
 
+import Data.List (nub)
+import Data.List.NonEmpty (NonEmpty((:|)), unfoldr)
 import Data.Word (Word64)
 import Test.Hspec
 import Polysemy
@@ -22,3 +24,19 @@ spec = describe "Random" $ do
                                   in ((int, real, word) : ns', qw)
                                ) ([], q0) (replicate 5 ())
     ns `shouldBe` reverse reference
+  it "`oneOf` shouldn't crash." $ do
+    five <- (nub <$>) . sequence . replicate 100 . runM . runRandomIO . oneOf $ (5 :: Int) :| []
+    five `shouldBe` [5]
+  it "`weighted` should give the only non-zero value." $ do
+    five <- (nub <$>) . sequence . replicate 100 . runM . runRandomIO . weighted @Int $ (0, 4) :| [(1, 5), (0, 6)]
+    five `shouldBe` [5]
+  it "`distributed` should work on infinite input." $ do
+    five <- (nub <$>) . sequence . replicate 100 . runM . runRandomIO . distributed @Int @Double $ unfoldr (\i -> ((i, 5), Just $ i / 2)) 0.5
+    five `shouldBe` [5]
+  it "`sample` should give the correct number of values" $ do
+    five <- (length <$>) . runM . runRandomIO $ sample @Bool @Int 5
+    five `shouldBe` 5
+  it "`sampleR` should give the correct number of values" $ do
+    five <- (length <$>) . runM . runRandomIO $ sampleR @Float @Int 5 (-10, 10)
+    five `shouldBe` 5
+
