81a4944456854f051c53133cae708727072ee7d8
Torsten Schmits <git@tryp.io>
2023-08-03 20:46:23 +0200

    release 0.8.2.0

--- flake.nix
+++ flake.nix
@@ -2,50 +2,58 @@
   description = "Experimental, user-contributed effects and interpreters for polysemy";
 
   inputs = {
-    nixpkgs_2009.url = github:nixos/nixpkgs/release-20.09;
-    nixpkgs_2105.url = github:nixos/nixpkgs/release-21.05;
-    unstable.url = github:nixos/nixpkgs/nixpkgs-unstable;
-    flake-utils.url = github:numtide/flake-utils;
+    nixpkgs.url = "github:nixos/nixpkgs/7e63eed145566cca98158613f3700515b4009ce3";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { nixpkgs_2009, nixpkgs_2105, unstable, flake-utils, ... }:
+  outputs = { nixpkgs, flake-utils, ... }:
   flake-utils.lib.eachSystem ["x86_64-linux"] (system:
-  with unstable.lib;
   let
-    hsPkgs = nixpkgs: compiler: import ./nix/overlay.nix { inherit system nixpkgs compiler; };
+    inherit (nixpkgs.lib) mapAttrs' nameValuePair composeManyExtensions versionAtLeast optionalAttrs;
+
+    pkgs = import nixpkgs { inherit system; };
+
+    src = pkgs.nix-gitignore.gitignoreSourcePure [./nix/source-filter] ./.;
+
+    basic = self: super: optionalAttrs (versionAtLeast super.ghc.version "9.4") {
+      ghcid = pkgs.haskell.lib.dontCheck super.ghcid;
+    } // {
+      polysemy-zoo = self.callCabal2nix "polysemy-zoo" src {};
+    };
+
+    hsPkgs = compiler: overrides: pkgs.haskell.packages.${compiler}.override {
+      overrides = composeManyExtensions ([basic] ++ overrides);
+    };
 
     ghcs = {
-      "865" = hsPkgs nixpkgs_2009 "ghc865";
-      "884" = hsPkgs nixpkgs_2105 "ghc884";
-      "8107" = hsPkgs unstable "ghc8107";
-      "901" = hsPkgs unstable "ghc901";
-      "921" = hsPkgs unstable "ghc921";
+      "810" = hsPkgs "ghc810" [];
+      "90" = hsPkgs "ghc90" [];
+      "92" = hsPkgs "ghc92" [];
+      "94" = hsPkgs "ghc94" [];
+      default = hsPkgs "ghc94" [(self: super: let
+        hackage = pkg: ver: sha256: self.callHackageDirect { inherit pkg ver sha256; } {};
+      in {
+        polysemy = hackage "polysemy" "1.9.1.0" "05mhzjz6hz0dnxsn3cc0l6yyj5ch35gn8xfnx0a1gn3q8yljfg2a";
+        polysemy-plugin = hackage "polysemy-plugin" "0.4.5.0" "0v2k0l42zaangwv050xfv5jdqfrbvdxfr533291ndsxalv8n3xi8";
+      })];
     };
 
-    mkPackage = version: {
-      "polysemy-zoo-${version}" = ghcs.${version}.polysemy-zoo;
-    };
+    package = version: ghc: nameValuePair "polysemy-zoo-${version}" ghc.polysemy-zoo;
 
-    packages =
-      foldl' (l: r: l // r) { inherit (ghcs."8107") polysemy-zoo; } (map mkPackage (attrNames ghcs));
+    packages = { inherit (ghcs.default) polysemy-zoo; default = ghcs.default.polysemy-zoo; } // mapAttrs' package ghcs;
 
-    mkDevShell = extra: ghc: ghc.shellFor {
+    devShell = ghc: ghc.shellFor {
       packages = p: [p.polysemy-zoo];
-      buildInputs = with ghc; [
-        cabal-install
-      ] ++ (if extra then [ghcid haskell-language-server] else []);
-      withHoogle = extra;
+      buildInputs = with ghc; [cabal-install ghcid haskell-language-server];
+      withHoogle = true;
     };
 
-    devShells = mapAttrs' (n: g: nameValuePair "ghc${n}" (mkDevShell (n != "921") g)) ghcs;
+    devShells = mapAttrs' (n: g: nameValuePair "ghc${n}" (devShell g)) ghcs // {
+      default = devShell ghcs.default;
+    };
 
   in {
     inherit packages devShells;
-
-    defaultPackage = packages.polysemy-zoo;
-
-    devShell = devShells.ghc8107;
-
     checks = packages;
   });
 }
